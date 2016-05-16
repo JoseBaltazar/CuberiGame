e3a87a137c866c5b56d270062e4e65d6445539e2
JoseBaltazar <a13c105@campustls.local>
2016-05-16 17:51:38 -0500

    Prueba de movimiento para plataformas
    Movimiento flotante para plataformas y partes externas del nivel

---
+++ Cuberi Game/Assets/Scripts/Obstaculo.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstaculo : MonoBehaviour {
+
+	int contador=0;
+	Vector3 posicion;
+
+	// Use this for initialization
+	void Start () {
+
+		posicion=transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MovimientoObstaculo();
+	}
+
+	void MovimientoObstaculo()
+	{
+		//movimiento arriba-abajo
+		if(gameObject.transform.tag=="ObsMovVertA")
+		{
+			if(contador<60)
+			{
+				posicion.y+=0.09f;
+				this.transform.position=posicion;
+				contador++;
+			}
+			else if(contador<120)
+			{
+				posicion.y-=0.09f;
+				this.transform.position=posicion;
+				contador++;
+			}
+			else
+			{
+				contador=0;
+			}
+		}
+		//movimiento abajo-arriba
+		if(gameObject.transform.tag=="ObsMovVertB")
+		{
+			if(contador<60)
+			{
+				posicion.y-=0.09f;
+				this.transform.position=posicion;
+				contador++;
+			}
+			else if(contador<120)
+			{
+				posicion.y+=0.09f;
+				this.transform.position=posicion;
+				contador++;
+			}
+			else
+			{
+				contador=0;
+			}
+		}
+		//movimiento izquierda-derecha
+		if(gameObject.transform.tag=="ObsMovHorizA")
+		{
+			if(contador<60)
+			{
+				posicion.x+=0.09f;
+				this.transform.position=posicion;
+				contador++;
+			}
+			else if(contador<120)
+			{
+				posicion.x-=0.09f;
+				this.transform.position=posicion;
+				contador++;
+			}
+			else
+			{
+				contador=0;
+			}
+		}
+	}
+
+}
---
+++ Cuberi Game/Assets/Scripts/ObstaculoMov.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstaculoMov : MonoBehaviour {
+
+    Vector3 inicio;
+    public int posicionY, velocidad, tiempo;
+
+	void Start () {
+
+        inicio = transform.position;
+
+	}
+	
+	void Update () {
+
+        transform.position = inicio;
+        inicio.y = Mathf.PingPong(velocidad * Time.time, tiempo) + posicionY;
+	
+	}
+}
