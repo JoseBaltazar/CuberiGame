4a912d2df3d4d5643dccffc6d84bff9a5ddcac0c
JoseBaltazar <joseba_97@hotmail.com>
2016-06-03 18:05:58 -0500

    Funcionamiento basico del boton Start

---
+++ Cuberi Game/Assets/Scripts/ButtonsMan.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonsMan : MonoBehaviour {
+
+    public GameObject StartBot;
+    public GameObject OptionsBot;
+
+	void Start () {
+	}
+	
+	void Update () {
+
+        CheckStart();
+	}
+
+    public void CheckStart()
+    {
+        if (GameObject.Find("Player").GetComponent<Player>().stop==false)
+        {
+            StartBot.SetActive(false);
+        }
+        if (GameObject.Find("Player").GetComponent<Player>().stop == true)
+        {
+            StartBot.SetActive(true);
+        }
+    }
+}
