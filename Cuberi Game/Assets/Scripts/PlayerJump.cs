0dfbf1de0533d9b2056e044214c5bae1cfd0191e
Gabo95pb <gabo.paz18.2@gmail.com>
2016-06-14 19:55:56 -0500

    scripts alternativos de salto
    sistema se fisica nuevo (para probar)

---
+++ Cuberi Game/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerJump : MonoBehaviour {
+    public float Pspeed;
+    float speed = 5f;
+    bool isGrounded;
+
+    public float Fsalto = 10.0f;
+    float appliedVerticalSpeed = 0;
+
+	// Use this for initialization
+	void Start () {
+        speed *= Pspeed;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+     Vector3 resultantVelocity = Genericphysincs.ApplyHorizontalVelocity(Vector3.right, speed);
+        resultantVelocity = Genericphysincs.ApplyGravity(resultantVelocity, ref appliedVerticalSpeed);
+        transform.Translate(resultantVelocity * Time.deltaTime);
+
+        CheckGround();
+        CheckJump(isGrounded);
+
+    }
+
+   
+
+    void CheckGround()
+    {
+        if (transform.position.y <= 0)
+        {
+            isGrounded = true;
+            ResetVerticalPosition();
+
+        }
+    }
+
+    void CheckJump(bool jumpEnabled)
+    {
+        if (jumpEnabled && Input.GetKeyDown(KeyCode.Space))
+        {
+            appliedVerticalSpeed = Fsalto;
+            isGrounded = false;
+        }
+    }
+
+    void ResetVerticalPosition()
+    {
+        Vector3 temp = transform.position;
+        temp.y = 0;
+        transform.position = temp;
+        appliedVerticalSpeed = 0;
+    }
+}
