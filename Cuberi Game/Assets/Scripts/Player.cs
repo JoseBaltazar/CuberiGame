548326ce2257e33be1318e9dcd503f456537b06d
JoseBaltazar <joseba_97@hotmail.com>
2016-06-24 16:25:08 -0500

    Adicion de documentacion 50%

--- Cuberi Game/Assets/Scripts/Player.cs
+++ Cuberi Game/Assets/Scripts/Player.cs
@@ -1,35 +1,41 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
+
+/*Clase desarrollada para otorgar movimiento a un objeto.
+ * dotarlo de un salto simulado, llevarlo a su posición inicial 
+ * y con posibilidad de controlarlo mediante teclas o entrada tactil
+ */
 
 public class Player : MonoBehaviour {
 
-    Vector3 inicio;
+    Vector3 inicio; /*Obtiene la posicion donde se encuentra actualmente el objeto*/
 
-    public int downTime;
-    public int upTime;
-    public int contador;
+    public int downTime; /*Define cuanto tiempo demora en bajar el objeto*/
+    public int upTime; /*Define cuanto tiempo demora en subir*/
+    public int contador; /*Define el movimiento en X del objeto dependiendo de su valor*/
 
-    public float speed;
+    public float speed; /*Variable que otorga velocidad constante al objeto*/
 
-    public bool right = false;
-    public bool left = false;
-    public bool stop;
-    public bool canJump = false;
-    public bool inTutorial;
+    public bool stop; /*Controla si el objeto puede moverse o no*/
+    public bool canJump = false; /*Controla si el metodo JumpCode() se ejecuta o no*/
+    public bool inTutorial; /*Define las acciones que puede hacer el objeto como desplazamiento o salto*/
 
 
     void Start()
     {
-
-        inicio = transform.position;
+        /*Se inicializan las variables para impedir el desplazamiento del objeto desde el inicio*/
         stop = true;
+
+        /*Igualamos la variable inicio a la posicion acutal del objeto*/
+        inicio = transform.position;
+
         upTime = 20;
 
     }
 
     void Update()
     {
+        /*Comprobamos los booleanos para realizar las acciones de movimieno o salto*/
 
         if (stop == false)
         {
@@ -43,20 +49,21 @@ public class Player : MonoBehaviour {
 
     }
 
+    /*Permite el movimiento del objeto cuando se ejecuta*/
     public void GoRun()
     {
         stop = false;
     }
 
+    /*Utilizado para desplazar el objeto en X cuando se ejecute*/
     void MovRun()
     {
         gameObject.transform.Translate(1 * Time.deltaTime * speed, 0, 0);
     }
 
+    /*Simula el salto y caída del objeto*/
     public void jumpCode()
     {
-
-
         if (upTime > 0)
         {
             transform.Translate(0, 10 * (Time.deltaTime), 0);
@@ -77,13 +84,9 @@ public class Player : MonoBehaviour {
                 upTime = 20;
             }
         }
-
-    }
-    public void Jump()
-    {
-        canJump = true;
     }
 
+    /*Metodos para controlar movimiento derecha-izquierda*/
     public void MoveRight()
     {
         if (contador < 1 && inTutorial == false)
@@ -101,6 +104,13 @@ public class Player : MonoBehaviour {
         }
     }
 
+    /*Convierte el valor del bool a verdadero*/
+    public void Jump()
+    {
+        canJump = true;
+    }
+
+    /*Recarga de la escena al morir*/
     public void Death()
     {
         SceneManager.LoadScene("Test Level");
