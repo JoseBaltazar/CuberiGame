5acac01c4009fc350ff1b5f1ef825fd1e18685b8
José Ripalda Jiménez <joseba_97@hotmail.com>
2016-07-10 18:44:37 -0500

    Prefabs de todos los niveles
    Tambien se crearon prefabs del Manager principal y los switches, el
    script Rots de eliminó y su contenido fue llevado a Collisions pensado
    en tener todas las colisiones del Player

---
+++ Cuberi Game/Assets/Scripts/Collisions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Collisions : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Rot0")
+        {
+            GameObject.Find("Player").GetComponent<Transform>().transform.Rotate(0, -90, 0);
+            Destroy(other.gameObject);
+        }
+        if (other.transform.tag == "Rot1")
+        {
+            GameObject.Find("Player").GetComponent<Transform>().transform.Rotate(0, 90, 0);
+            Destroy(other.gameObject);
+        }
+        if (other.transform.tag == "Rot2")
+        {
+            GameObject.Find("Player").GetComponent<Transform>().transform.Rotate(0, -90, 0);
+            Destroy(other.gameObject);
+        }
+        if (other.transform.tag == "Rot3")
+        {
+            GameObject.Find("Player").GetComponent<Transform>().transform.Rotate(0, 0, 90);
+            Destroy(other.gameObject);
+        }
+        if (other.transform.tag == "Rot4")
+        {
+            GameObject.Find("Player").GetComponent<Transform>().transform.Rotate(0, 0, -90);
+            Destroy(other.gameObject);
+        }
+
+    }
+}
