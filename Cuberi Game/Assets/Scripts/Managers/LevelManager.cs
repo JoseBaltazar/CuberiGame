7fe590884db3d1ec44f0024b9a3ec36ac4fc739d
José Ripalda Jiménez <joseba_97@hotmail.com>
2016-07-11 01:47:23 -0500

    Cambios menores
    Adicion de variables para niveles 3 y 4

--- Cuberi Game/Assets/Scripts/Managers/LevelManager.cs
+++ Cuberi Game/Assets/Scripts/Managers/LevelManager.cs
@@ -4,19 +4,26 @@ public class LevelManager : MonoBehaviour {
 
 	public GameObject level1;
 	public GameObject level2;
+    public GameObject level3;
+    public GameObject level4;
+
     public GameObject tLevel;
 
-	public bool tActive,l1Active, l2Active;
+	public bool tActive,l1Active, l2Active, l3Active, l4Active;
 
 	void Start () {
 
         tLevel = GameObject.FindGameObjectWithTag("Tutorial");
 		level1 = GameObject.FindGameObjectWithTag("Level1");
 		level2 = GameObject.FindGameObjectWithTag("Level2");
+        level3 = GameObject.FindGameObjectWithTag("Level3");
+        level4 = GameObject.FindGameObjectWithTag("Level4");
 
         tLevel.SetActive(true);
         level1.SetActive(false);
         level2.SetActive(false);
+        level3.SetActive(false);
+        level4.SetActive(false);
 
     }
 
@@ -36,6 +43,7 @@ public class LevelManager : MonoBehaviour {
 		{
 			level1.SetActive(false);
 		}
+
         if (l2Active == true)
         {
             level2.SetActive(true);
@@ -44,6 +52,25 @@ public class LevelManager : MonoBehaviour {
         {
             level2.SetActive(false);
         }
+
+        if (l3Active == true)
+        {
+            level3.SetActive(true);
+        }
+        if (l3Active == false)
+        {
+            level3.SetActive(false);
+        }
+
+        if (l4Active == true)
+        {
+            level4.SetActive(true);
+        }
+        if (l4Active == false)
+        {
+            level4.SetActive(false);
+        }
+
         if (tActive == true)
         {
             tLevel.SetActive(true);
